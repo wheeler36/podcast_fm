9277b1ebcc60abd542939951d4b2aa9c20fbb102
wheeler36 <wheeler.hord@gmail.com>
2016-07-11 23:25:06 -0800

    Add authorization to podcast episodes

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,4 +1,6 @@
 class EpisodesController < ApplicationController
+  before_action :authenticate_podcast!, except: [:show]
+  before_filter :require_permission, except: [:show]
   before_action :find_podcast
   before_action :find_episode, only: [:show, :edit, :update, :destroy]
 
@@ -47,5 +49,12 @@ class EpisodesController < ApplicationController
 
   def episode_params
     params.require(:episode).permit(:title, :description)
+  end
+
+  def require_permission
+    @podcast = Podcast.find(params[:podcast_id])
+    if current_podcast != @podcast
+      redirect_to root_path, notice: "Sorry, you're not allowed to view that page."
+    end
   end
 end
